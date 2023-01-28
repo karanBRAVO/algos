f824935d5cf927d82fd2dd8c4b04988930f7e12a
karan yadav <xpresskaran@gmail.com>
2023-01-28 18:56:02 +0530

    added new files

---
+++ sudoku_solver/main2.py
@@ -0,0 +1,13 @@
+import main as bd
+
+
+# function to print the board
+def show_output(lst):
+    for item in lst:
+        print(item)
+
+
+# printing the given board
+if __name__ == "__main__":
+    show_output(bd.BOARD)
+
